85d211400cfc95d5617d4569ce3d0a68b3b138cf
paracamplus <gerant@paracamplus.com>
2021-04-05 14:35:52 +0200

    Deployed on 20210405_123552

---
+++ docs/client/legacy/utils.6bb646a3.js
@@ -0,0 +1,1 @@
+import"./client.3dfb36a3.js";function e(){return"undefined"==typeof window?"server":"client"}export{e as w};
---
+++ docs/client/utils.932e656f.js
@@ -0,0 +1,1 @@
+function e(){return"undefined"==typeof window?"server":"client"}export{e as w};
